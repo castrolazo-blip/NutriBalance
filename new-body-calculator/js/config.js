855b2bfff6af7b1f3cb1e71d848bb8e1ff825a99
castrolazo-blip <castro_lazo@yahoo.es>
2026-04-25 15:29:58 -0600

    Update config.js

--- new-body-calculator/js/config.js
+++ new-body-calculator/js/config.js
@@ -7,7 +7,7 @@
 
 window.APP_CONFIG = {
     SUPABASE_URL: 'https://rpdoacfxuizpgdpnfklq.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwZG9hY2Z4dWl6cGdkcG5ma2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MTIzNjksImV4cCI6MjA5MjM4ODM2OX0.ZLGRkTFGZcdaZMzOjQbj4IjH8-cBFg9Eir8Ye-rhUTE',
+    SUPABASE_ANON_KEY: 'sb_publishable_V6NvqXf7Gn_OPCYanZzFow_qq86Ur82',
 
     APP_NAME: 'New Body Calculator',
     APP_VERSION: '1.0.0',
